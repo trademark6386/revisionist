6dcc619019b8c031f2fb3d75706c3b9f2445e45d
MarMarBunbun <marcosegg.88@gmail.com>
2023-10-18 14:32:47 -0600

    New Abilities Showdown side
    Added 9 Abilities
    Original - Terrorize
    Original - Digitize
    Original - Fossilize
    SAGE - Orbital Tide
    SAGE - Perforate
    SAGE - Permafrost
    SAGE - Pollution
    SAGE - Scavenger
    SAGE - Sunbathe
    
    Added tags to showdown folder to start testing other abilities

--- Gravels Extended Battles/fabric/src/main/resources/drai/dev/gravelmon/fabric/abilities.js
+++ Gravels Extended Battles/fabric/src/main/resources/drai/dev/gravelmon/fabric/abilities.js
@@ -39,6 +39,164 @@ const Abilities = {
     name: "Damp",
     rating: 0.5,
     num: 6
+  },
+  digitize: {
+    onModifyTypePriority: -1,
+    onModifyType(move, pokemon) {
+      const noModifyType = [
+        "judgment",
+        "multiattack",
+        "naturalgift",
+        "revelationdance",
+        "technoblast",
+        "terrainpulse",
+        "weatherball"
+      ];
+      if (move.type === "Normal" && !noModifyType.includes(move.id) && !(move.isZ && move.category !== "Status") && !(move.name === "Tera Blast" && pokemon.terastallized)) {
+        move.type = "Digital";
+        move.typeChangerBoosted = this.effect;
+      }
+    },
+    onBasePowerPriority: 23,
+    onBasePower(basePower, pokemon, target, move) {
+      if (move.typeChangerBoosted === this.effect)
+        return this.chainModify([4915, 4096]);
+    },
+    name: "Digitize",
+    rating: 4,
+    num: 307
+  },
+  fossilize: {
+    onModifyTypePriority: -1,
+    onModifyType(move, pokemon) {
+      const noModifyType = [
+        "judgment",
+        "multiattack",
+        "naturalgift",
+        "revelationdance",
+        "technoblast",
+        "terrainpulse",
+        "weatherball"
+      ];
+      if (move.type === "Normal" && !noModifyType.includes(move.id) && !(move.isZ && move.category !== "Status") && !(move.name === "Tera Blast" && pokemon.terastallized)) {
+        move.type = "Rock";
+        move.typeChangerBoosted = this.effect;
+      }
+    },
+    onBasePowerPriority: 23,
+    onBasePower(basePower, pokemon, target, move) {
+      if (move.typeChangerBoosted === this.effect)
+        return this.chainModify([4915, 4096]);
+    },
+    name: "Fossilize",
+    rating: 4,
+    num: 308
+  },
+  orbitaltide: {
+    onStart(source) {
+      this.field.setPseudoWeather("gravity");
+    },
+    name: "Orbital Tide",
+    rating: 4,
+    num: 299
+  },
+  perforate: {
+    onModifyMovePriority: -5,
+    onModifyMove(move) {
+      if (!move.ignoreImmunity)
+        move.ignoreImmunity = {};
+      if (move.ignoreImmunity !== true) {
+        move.ignoreImmunity["Poison"] = true;
+        move.ignoreImmunity["Ghost"] = true;
+		move.ignoreImmunity["Flying"] = true;
+      }
+    },
+    name: "Perforate",
+    rating: 3,
+    num: 300
+  },
+  permafrost: {
+    onModifyDef(def, pokemon) {
+      if (this.field.isWeather(["hail", "snow"])) {
+        return this.chainModify(1.5);
+      }
+    },
+    name: "Permafrost",
+    rating: 3,
+    num: 301
+  },
+  pollution: {
+    onStart(source) {
+      this.field.setWeather("acidrain");
+    },
+    name: "Pollution",
+    rating: 4,
+    num: 302
+  },
+  psychout: {
+    onStart(pokemon) {
+      let activated = false;
+      for (const target of pokemon.adjacentFoes()) {
+        if (!activated) {
+          this.add("-ability", pokemon, "Intimidate", "boost");
+          activated = true;
+        }
+        if (target.volatiles["substitute"]) {
+          this.add("-immune", target);
+        } else {
+          this.boost({ spa: -1 }, target, pokemon, null, true);
+        }
+      }
+    },
+    name: "Intimidate",
+    rating: 3.5,
+    num: 303
+  },
+  scavenger: {
+    onSourceAfterFaint(length, pokemon, source, effect) {
+      if (effect && effect.effectType === "Move") {
+        this.pokemon.heal(pokemon.baseMaxhp / 4);
+      }
+    },
+    name: "Scavenger",
+    rating: 3.5,
+    num: 304
+  },
+  sunbathe: {
+    onWeather(target, source, effect) {
+      if (effect.id === "sunnyday" || effect.id === "desolateland") {
+        this.heal(target.baseMaxhp / 16);
+      }
+    },
+    name: "Sunbathe",
+    rating: 1,
+    num: 305
+  },
+  terrorize: {
+    onModifyTypePriority: -1,
+    onModifyType(move, pokemon) {
+      const noModifyType = [
+        "judgment",
+        "multiattack",
+        "naturalgift",
+        "revelationdance",
+        "technoblast",
+        "terrainpulse",
+        "weatherball"
+      ];
+      if (move.type === "Normal" && !noModifyType.includes(move.id) && !(move.isZ && move.category !== "Status") && !(move.name === "Tera Blast" && pokemon.terastallized)) {
+        move.type = "Ghost";
+        move.typeChangerBoosted = this.effect;
+      }
+    },
+    onBasePowerPriority: 23,
+    onBasePower(basePower, pokemon, target, move) {
+      if (move.typeChangerBoosted === this.effect)
+        return this.chainModify([4915, 4096]);
+    },
+    name: "Terrorize",
+    rating: 4,
+    num: 306
   }
 };
 //# sourceMappingURL=abilities.js.map
