a2ec192cd2b50020365a48854ce0c8f04c20a063
StijnArts <JKT.Arts@student.han.nl>
2023-10-28 11:22:52 +0200

    bugfixes and spawn checking command

---
+++ Gravels Extended Battles/fabric/src/main/java/drai/dev/gravelsextendedbattles/fabric/GravelmonCommands.java
@@ -0,0 +1,12 @@
+package drai.dev.gravelsextendedbattles.fabric;
+
+import com.mojang.brigadier.*;
+import net.minecraft.command.*;
+import net.minecraft.server.command.*;
+
+public class GravelmonCommands {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registry,
+                                CommandManager.RegistrationEnvironment selection ){
+        CheckSpawnsForSpeciesCommand.register(dispatcher);
+    }
+}
